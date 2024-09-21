385ad9cca944dd09cc36ebe2798e0b8e3fa4cd2e
sadeval <162736837+sadeval@users.noreply.github.com>
2024-09-21 10:17:03 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+        Process process = new Process();
+
+        process.StartInfo.FileName = @"C:\Program Files\Microsoft Office\root\Office16\WINWORD.EXE";
+
+        process.Start();
+
+        Console.WriteLine("Word запущен, ожидание завершения...");
+
+        process.WaitForExit();
+
+        int exitCode = process.ExitCode;
+        Console.WriteLine($"Процесс завершен с кодом: {exitCode}");
+    }
+}
